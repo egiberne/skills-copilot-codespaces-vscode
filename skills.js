88a8905fc26ae991cc5db5abb3c9874d6bb7eef0
egiberne <egiberne@gmail.com>
2024-05-30 22:11:12 -0400

    updated

--- skills.js
+++ skills.js
@@ -2,4 +2,7 @@ function calculateNumbers(var1, var2) {
   return var1 + var2;
 }
 
-
+// Path: index.js
+const numbers = require('./skills');
+console.log(numbers.calculateNumbers(1, 2)); 
+// Output: 3
