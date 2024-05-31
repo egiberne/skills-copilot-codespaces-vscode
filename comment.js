d1bbb92449f6cd7d41515f28078096583df6e9bf
egiberne <egiberne@gmail.com>
2024-05-30 21:54:17 -0400

    Change the code

--- comment.js
+++ comment.js
@@ -1,53 +1,66 @@
 //create web server
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const fs = require('fs');
-
-//create a server
-app.listen(3000, function(){
-    console.log('Server is running on port 3000');
-});
-
-//use body parser
+var express = require('express');
+var app = express();
+var fs = require('fs');
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+var cookieParser = require('cookie-parser');
+app.use(cookieParser());
+app.use(express.static('public'));
 app.use(bodyParser.json());
-
-//get all comments
-app.get('/comments', function(req, res){
-    fs.readFile('comments.json', function(err, data){
-        if(err){
-            console.log(err);
-            res.status(500).send('Server error');
-        }
-        res.send(JSON.parse(data));
-    });
+//create server
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var path = require('path');
+var mysql = require('mysql');
+//connect to database
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "mydb"
 });
-
-//get a single comment
-app.get('/comments/:id', function(req, res){
-    fs.readFile('comments.json', function(err, data){
-        if(err){
-            console.log(err);
-            res.status(500).send('Server error');
-        }
-        let comments = JSON.parse(data);
-        let comment = comments.find(comment => comment.id == req.params.id);
-        if(comment){
-            res.send(comment);
-        } else {
-            res.status(404).send('Comment not found');
-        }
-    });
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
 });
-
-//add a comment
-app.post('/comments', function(req, res){
-    fs.readFile('comments.json', function(err, data){
-        if(err){
-            console.log(err);
-            res.status(500).send('Server error');
-        }
-        let comments = JSON.parse(data);
-        let newComment = {
-            id: req.body.id,
-            message: req.body.message,
+//get comment from database
+app.get('/getComment', function(req, res) {
+  var sql = "SELECT * FROM comment";
+  con.query(sql, function(err, result) {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+//post comment to database
+app.post('/postComment', urlencodedParser, function(req, res) {
+  var sql = "INSERT INTO comment (name, comment) VALUES ('" + req.body.name + "','" + req.body.comment + "')";
+  con.query(sql, function(err, result) {
+    if (err) throw err;
+    console.log("1 record inserted");
+  });
+  res.send('Comment posted');
+});
+//delete comment from database
+app.post('/deleteComment', urlencodedParser, function(req, res) {
+  var sql = "DELETE FROM comment WHERE name = '" + req.body.name + "' AND comment = '" + req.body.comment + "'";
+  con.query(sql, function(err, result) {
+    if (err) throw err;
+    console.log("1 record deleted");
+  });
+  res.send('Comment deleted');
+});
+//get comment from database
+app.get('/getComment', function(req, res) {
+  var sql = "SELECT * FROM comment";
+  con.query(sql, function(err, result) {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+//send comments to client
+io.on('connection', function(socket) {
+  socket.on('getComment', function() {
+    var sql = "SELECT * FROM comment";
+    con.query(sql, function(err, result) {
+      if (err) throw err
